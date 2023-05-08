c44bd3015f76b4504804efeb2990f732476eed61
Nash Addams <nashaddams>
2023-05-08 22:07:40 +0200

    feat!: facilitate f1 race schedule import
    BREAKING CHANGE: The store has been extended to accommodate F1 race
    schedules. Older backups won't be restorable.

---
+++ js/types.js
@@ -0,0 +1,66 @@
+/**
+ * Object for show and movie search result (and F1 import) items
+ */
+class ResultItem {
+  constructor({
+    id = undefined,
+    image = './img/placeholder.svg',
+    name = undefined,
+    releaseDate = undefined,
+  }) {
+    this.id = id,
+    this.image = image;
+    this.name = name;
+    this.releaseDate = releaseDate;
+  }
+}
+
+/**
+ * Object for saved show and movie items
+ */
+class SavedItem {
+  constructor({
+    id = undefined,
+    image = './img/placeholder.svg',
+    name = undefined,
+    releaseDate = undefined,
+    details = undefined,
+    status = undefined,
+    genres = [],
+    episodes = [],
+    imdbUrl = undefined,
+  }) {
+    this.id = id,
+    this.image = image;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.details = details;
+    this.status = status;
+    this.genres = genres;
+    this.episodes = episodes;
+    this.imdbUrl = imdbUrl;
+  }
+}
+
+/**
+ * Object for show episodes, movies, and F1 items
+ */
+class RecentUpcomingItem {
+  constructor({
+    id = undefined,
+    image = './img/placeholder.svg',
+    name = undefined,
+    releaseDate = undefined,
+    episodeNumber = undefined,
+    seasonNumber = undefined,
+    showName = undefined,
+  }) {
+    this.id = id,
+    this.image = image;
+    this.name = name;
+    this.releaseDate = releaseDate;
+    this.episodeNumber = episodeNumber;
+    this.seasonNumber = seasonNumber;
+    this.showName = showName;
+  }
+}
